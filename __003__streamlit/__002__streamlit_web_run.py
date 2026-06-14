cb42760d466196e9274b8ab31b07cba1ebb945e2
湖北蔡徐坤 <19013806629@163.com>
2026-06-14 16:57:41 +0800

    chore(config): 更新项目路径配置
    - 修改 streamlit 运行脚本中的项目路径
    - 将旧的武汉黑马项目路径替换为新的智能营销代理项目路径
    - 确保路径指向正确的 streamlit web 应用文件

--- __003__streamlit/__002__streamlit_web_run.py
+++ __003__streamlit/__002__streamlit_web_run.py
@@ -1,4 +1,4 @@
 import sys
 import os
 
-os.system(r"streamlit run D:\HeiMa\讲课相关\武汉黑马03期资料\大模型课程共享\01-code\wuhan3_auto_marketing_project\__003__streamlit\__001__streamlit_web.py")
+os.system(r"streamlit run D:\IT_Heima_Project\Smart_Marketing_Agent\__003__streamlit\__001__streamlit_web.py")
